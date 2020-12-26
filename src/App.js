f009f493098ee7f6a2b37e7913d1594ceec0155a
Debbie Cheng <dqrcheng@gmail.com>
2020-12-26 14:37:40 +1100

    input

--- src/App.js
+++ src/App.js
@@ -1,50 +1,17 @@
 import React from 'react';
 import './App.css';
+import HexInput from './component/HexInput';
 
 function App() {
-  const hexRegex = /^#[0-9A-F]{6}$/i;
-  const defaultBg = '#eeeeee';
-  const [bgInput, setBgInput] = React.useState(defaultBg);
-  const [bg, setBg] = React.useState(defaultBg);
-  const [err, setErr] = React.useState(false);
-
-  React.useEffect(() => {
-    document.body.style.background = bg;
-  }, [bg]);
-
-  let updateTimeout = null;
-  const handleBgChange = (e) => {
-    setBgInput(e.target.value);
-    clearTimeout(updateTimeout);
-    updateTimeout = setTimeout(() => {
-      if (hexRegex.test(e.target.value)) {
-        setErr(false);
-        setBg(e.target.value);
-      } else {
-        setErr(true);
-        setBg(defaultBg);
-      }
-    }, 3000);
-  }
-
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hi</h1>
+      <h1 id="fg-text">Hello World</h1>
       </header>
-      <div className="input-field">
-      <label>Background
-        <input
-            type="text"
-            name="bg-colour"
-            value={bgInput}
-            maxLength="7"
-            className="text-input"
-            onChange={handleBgChange}
-          />
-        </label>
-        {err ? <div className="error-message">Invalid hex code!</div> : null}
-        </div>
+      <main>
+      {/* <HexInput labelText="text" field="foreground" /> */}
+      <HexInput labelText="background" field="background"/>
+      </main>
     </div>
   );
 }
---
+++ src/component/HexInput.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+
+const HexInput = ({ labelText, field }) => {
+  let updateTimeout = null;
+  const hexRegex6 = /^#[0-9A-F]{6}$/i;
+  const hexRegex3 = /^#([0-9a-f]{3}){1,2}$/i;
+
+  const [fieldType] = useState(field);
+  const [defaultColour, setDefault] = React.useState('#eeeeee');
+  const [colourInput, setColourInput] = React.useState(defaultColour);
+  const [colour, setColour] = React.useState(defaultColour);
+  const [err, setErr] = React.useState(false);
+
+  React.useEffect(() => {
+    if (fieldType === 'background') {
+      document.body.style.background = colour;
+    }
+    else if (fieldType === 'foreground') {
+      document.getElementById('fg-text').style.color = colour;
+    }
+  }, [colour, fieldType]);
+
+  const handleChange = (e) => {
+    setColourInput(e.target.value);
+    clearTimeout(updateTimeout);
+    updateTimeout = setTimeout(() => {
+      if (hexRegex6.test(e.target.value) || hexRegex3.test(e.target.value)) {
+        setErr(false);
+        setDefault(e.target.value);
+        setColour(e.target.value);
+      } else {
+        setErr(true);
+        setColour(defaultColour);
+      }
+    }, 1000);
+  }
+
+  return (
+    <div className="input-field">
+      <label>
+        {labelText}
+        <input
+          type="text"
+          name="colour"
+          value={colourInput}
+          maxLength="7"
+          className="text-input"
+          autoComplete="off"
+          onChange={handleChange}
+        />
+      </label>
+      {err ? <div className="error-message">Invalid hex code!</div> : null}
+    </div>
+  )
+}
+
+export default HexInput;
