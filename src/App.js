e598d5aefd75994d58a030d426d9bbc218240eb5
Debbie Cheng <dqrcheng@gmail.com>
2020-12-26 16:29:40 +1100

    Media query for responsiveness

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import HexInput from './component/HexInput';
 function App() {
   return (
     <div className="App">
-      <div className="text-demo">
-        <h1 id="fg-heading">Hello World</h1>
-        <p id="fg-paragraph">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque blandit consequat
-          congue. Donec eleifend laoreet turpis vel laoreet. Cras finibus tellus in dui euismod,
-          quis laoreet purus interdum. Donec vestibulum tincidunt odio, vel gravida justo ultrices in.
-          Vivamus sodales odio sit amet risus hendrerit, eu finibus mi viverra. Maecenas mollis felis
-          ante, ornare luctus nisl efficitur id. Curabitur eget nisl risus. Morbi non dapibus odio.
-          Morbi porta, erat a interdum tincidunt, dolor dolor laoreet nisi, in mattis ligula urna quis
-          ex.
+      <div className="text-wrapper">
+        <div className="text-demo">
+          <h1 id="fg-heading">Hello World</h1>
+          <p id="fg-paragraph">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque blandit consequat
+            congue. Donec eleifend laoreet turpis vel laoreet. Cras finibus tellus in dui euismod,
+            quis laoreet purus interdum. Donec vestibulum tincidunt odio, vel gravida justo ultrices in.
+            Vivamus sodales odio sit amet risus hendrerit, eu finibus mi viverra. Maecenas mollis felis
+            ante, ornare luctus nisl efficitur id. Curabitur eget nisl risus. Morbi non dapibus odio.
+            Morbi porta, erat a interdum tincidunt, dolor dolor laoreet nisi, in mattis ligula urna quis
+            ex.
       </p>
+        </div>
       </div>
       <div className="input-wrapper">
         <HexInput labelText="text" field="foreground" />
